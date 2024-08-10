5b61b054d78e74cb2f0b4bf4c530b8f6cd9be3e2
Jan Kotvaš <jan.kotvas96@gmail.com>
2024-08-10 22:04:17 +0200

    Added the password strength check and visualization to the DOM

--- app.js
+++ app.js
@@ -2,15 +2,97 @@ const slider = document.getElementById("character-length");
 const sliderValueDisplay = document.getElementById("character-range");
 const submitBtn = document.getElementById("submitForm");
 const form = document.getElementById("form");
+const passwordStrength = document.getElementById("character-length");
+const checkboxes = document.querySelectorAll("input[type=checkbox]");
+const passwordStrengthEl = document.getElementById("password-strength");
+const passwordStrengthIndicatorEl = document.getElementById(
+  "password-strength-indicator"
+);
 
+// passwordStrengthEl.innerText = "";
 sliderValueDisplay.innerText = slider.value;
+
+const getFormData = () => {
+  const passwordStrengthValue = parseInt(passwordStrength.value);
+  let isAnyCheckboxChecked = false;
+
+  checkboxes.forEach((checkbox) => {
+    if (checkbox.checked) {
+      console.log(checkbox.name);
+      isAnyCheckboxChecked = true;
+    }
+  });
+
+  console.log(passwordStrengthValue);
+
+  return {
+    passwordStrengthValue,
+    isAnyCheckboxChecked,
+  };
+};
+
+const passwordStrengthVisualiziation = (passwordStrength) => {
+  passwordStrengthIndicatorEl.innerHTML = "";
+
+  let bars = createBars();
+
+  let numColoredBars;
+  let strengthClass;
+
+  if (passwordStrength < 3) {
+    passwordStrengthEl.innerText = "TOO WEAK";
+    numColoredBars = 1;
+    strengthClass = "too-weak";
+  } else if (passwordStrength >= 3 && passwordStrength < 5) {
+    passwordStrengthEl.innerText = "WEAK";
+    numColoredBars = 2;
+    strengthClass = "weak";
+  } else if (passwordStrength >= 5 && passwordStrength < 8) {
+    passwordStrengthEl.innerText = "MEDIUM";
+    numColoredBars = 3;
+    strengthClass = "medium";
+  } else if (passwordStrength >= 8) {
+    passwordStrengthEl.innerText = "STRONG";
+    numColoredBars = 4;
+    strengthClass = "strong";
+  }
+
+  applyBarColoring(bars, numColoredBars, strengthClass);
+};
+
+const createBars = () => {
+  let bars = 4;
+  const barElements = [];
+
+  for (let i = 1; i <= bars; i++) {
+    const bar = document.createElement("div");
+    bar.classList.add("bar");
+
+    passwordStrengthIndicatorEl.appendChild(bar);
+
+    barElements.push(bar);
+  }
+
+  return barElements;
+};
+
+const applyBarColoring = (bars, numColoredBars, strengthClass) => {
+  bars.forEach((bar) => {
+    bar.classList.remove("too-weak", "weak", "medium", "strong");
+    bar.classList.add("bar");
+  });
+
+  for (let i = 0; i < numColoredBars; i++) {
+    bars[i].classList.add(strengthClass);
+  }
+};
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
-  const formData = new FormData(form);
-  for (const [key, value] of formData) {
-    console.log(`${key} - ${value}`);
-  }
+
+  let { passwordStrengthValue, isAnyCheckboxChecked } = getFormData();
+
+  passwordStrengthVisualiziation(passwordStrengthValue);
 });
 
 slider.addEventListener("input", () => {
