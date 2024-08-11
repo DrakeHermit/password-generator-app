e280d85262876ece97725760d13c2a82b31254e9
Jan Kotvaš <jan.kotvas96@gmail.com>
2024-08-11 20:34:42 +0200

    Added password generation based on one checkbox being checked

--- app.js
+++ app.js
@@ -8,26 +8,23 @@ const passwordStrengthEl = document.getElementById("password-strength");
 const passwordStrengthIndicatorEl = document.getElementById(
   "password-strength-indicator"
 );
+const passwordField = document.getElementById("password");
 
-// passwordStrengthEl.innerText = "";
 sliderValueDisplay.innerText = slider.value;
 
 const getFormData = () => {
   const passwordStrengthValue = parseInt(passwordStrength.value);
-  let isAnyCheckboxChecked = false;
+  let checkedCheckboxes = [];
 
   checkboxes.forEach((checkbox) => {
     if (checkbox.checked) {
-      console.log(checkbox.name);
-      isAnyCheckboxChecked = true;
+      checkedCheckboxes.push(checkbox.name);
     }
   });
 
-  console.log(passwordStrengthValue);
-
   return {
     passwordStrengthValue,
-    isAnyCheckboxChecked,
+    checkedCheckboxes,
   };
 };
 
@@ -40,18 +37,18 @@ const passwordStrengthVisualiziation = (passwordStrength) => {
   let strengthClass;
 
   if (passwordStrength < 3) {
-    passwordStrengthEl.innerText = "TOO WEAK";
+    passwordStrengthEl.innerText = "TOO WEAK!";
     numColoredBars = 1;
     strengthClass = "too-weak";
   } else if (passwordStrength >= 3 && passwordStrength < 5) {
     passwordStrengthEl.innerText = "WEAK";
     numColoredBars = 2;
     strengthClass = "weak";
-  } else if (passwordStrength >= 5 && passwordStrength < 8) {
+  } else if (passwordStrength >= 5 && passwordStrength < 10) {
     passwordStrengthEl.innerText = "MEDIUM";
     numColoredBars = 3;
     strengthClass = "medium";
-  } else if (passwordStrength >= 8) {
+  } else if (passwordStrength >= 10) {
     passwordStrengthEl.innerText = "STRONG";
     numColoredBars = 4;
     strengthClass = "strong";
@@ -87,12 +84,69 @@ const applyBarColoring = (bars, numColoredBars, strengthClass) => {
   }
 };
 
+const randomCharacters = (passwordLength, checkedCheckbox) => {
+  const uppercaseLetters = "ABCDEFHIJKLMNOPQRSTUVWXYZ";
+  const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+  const numbers = "0123456789";
+  const symbols = "@!%$&*/?#$^,.|";
+  const randomCharacters = [];
+
+  if (checkedCheckbox.includes("uppercase")) {
+    for (i = 1; i <= passwordLength; i++) {
+      const randomUppercaseLetters = uppercaseLetters.charAt(
+        Math.floor(Math.random() * uppercaseLetters.length)
+      );
+      randomCharacters.push(randomUppercaseLetters);
+    }
+  } else if (checkedCheckbox.includes("lowercase")) {
+    for (i = 1; i <= passwordLength; i++) {
+      const randomLowercaseLetters = lowercaseLetters.charAt(
+        Math.floor(Math.random() * lowercaseLetters.length)
+      );
+      randomCharacters.push(randomLowercaseLetters);
+    }
+  } else if (checkedCheckbox.includes("numbers")) {
+    for (i = 1; i <= passwordLength; i++) {
+      const randomNumbers = numbers.charAt(
+        Math.floor(Math.random() * numbers.length)
+      );
+      randomCharacters.push(randomNumbers);
+    }
+  } else if (checkedCheckbox.includes("symbols")) {
+    for (i = 1; i <= passwordLength; i++) {
+      const randomSymbols = symbols.charAt(
+        Math.floor(Math.random() * symbols.length)
+      );
+      randomCharacters.push(randomSymbols);
+    }
+  }
+
+  const randomLettersStr = randomCharacters.join("");
+
+  return randomLettersStr;
+};
+
+const updatePasswordDisplay = (string) => {
+  if (string !== "") {
+    passwordField.innerText = string;
+  } else {
+    passwordField.innerText = "Select character length";
+    passwordField.style.fontSize = "18px";
+  }
+  if (passwordField.classList.contains("inactive")) {
+    passwordField.style.color = "#fff";
+  }
+};
+
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
-  let { passwordStrengthValue, isAnyCheckboxChecked } = getFormData();
+  let { passwordStrengthValue, checkedCheckboxes } = getFormData();
 
   passwordStrengthVisualiziation(passwordStrengthValue);
+
+  let password = randomCharacters(passwordStrengthValue, checkedCheckboxes);
+  updatePasswordDisplay(password);
 });
 
 slider.addEventListener("input", () => {
@@ -102,6 +156,8 @@ slider.addEventListener("input", () => {
   sliderValueDisplay.innerText = currentSliderValue;
 
   let sliderPosition = (currentSliderValue / maxSliderValue) * 100;
-  let sliderGradient = `linear-gradient(90deg ,var(--accent-color) ${sliderPosition}%, var(--background-color) ${sliderPosition}%)`;
+  let sliderGradient = `linear-gradient(90deg ,var(--accent-color) ${sliderPosition}%, var(--background-color) ${
+    sliderPosition + 1
+  }%)`;
   slider.style.background = sliderGradient;
 });
